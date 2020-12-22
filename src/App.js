731a8913854129aa5a6dd14533f24c129dac4ba8
Yashita Jain <yashitarjain16@gmail.com>
2020-12-22 14:55:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = { advice: '' };//global object
+
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const { advice } = response.data.slip;
+                this.setState({ advice: advice });
+            }
+            )
+            .catch((error) => {
+                console.log(error);
+            }
+            )
+    }
+
+    render() {
+        const { advice } = this.state;
+        return (
+            <div className="bg">
+                <div className="card text-center border shadow">
+                    <h6 className="card-header text-primary">
+                        Advice Box
+                    </h6>
+                    <div className="card-body">
+                        <h4 className="card-title text-danger">{advice}</h4>
+                        <button className="btn btn-primary" onClick={this.fetchAdvice}>Need More Advice?</button>
+                    </div>
+                    <div className="card-footer text-success">
+                        Think Positive!
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-
 export default App;
